7560baf3f080847bd4f7f03ac0768643a1cc464b
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-08-08 23:41:15 +0300

    Add clear all & backspace buttons

--- script.js
+++ script.js
@@ -46,37 +46,52 @@ function operate() {
 const digits = Array.from(document.getElementsByClassName('digit'));
 const operators = Array.from(document.getElementsByClassName('operator'));
 const equals = document.querySelector('#equals');
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clearBtn');
+const backspaceBtn = document.querySelector('.backSpace');
+
+display.textContent = '0';
 
 // Array for storing the current & next number & the operator
-const myArray = ['', '', ''];
+const myArray = ['0', '', ''];
    
-
 digits.forEach((digit) => {
     digit.addEventListener('click', (e) => {
-        if(typeof(myArray[0]) != 'number') {
+        if((`${e.target.getAttribute('id')}` == '0') && (myArray[0] == '0') && (!myArray[2])) {
+            return;
+        } else if(typeof(myArray[0]) != 'number' || !myArray[1]) {
+            if(myArray[0] == '0' || (typeof(myArray[0]) == 'number' && !myArray[1])) {
+                myArray[0] = '';
+            }
             myArray[0] += `${e.target.getAttribute('id')}`;
             console.log(myArray[0]);
+            display.textContent = `${myArray[0]}`;
         } else {
+            if(myArray[2] == '0') {
+                myArray[2] = '';
+            }
             myArray[2] += `${e.target.getAttribute('id')}`;
             console.log(myArray[2]);
+            display.textContent = `${myArray[2]}`;
         }
     });
 });
 
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
-        if(myArray[1] == '/' && (myArray[2])) {
-            console.log('Error');
+        if(myArray[1] == '/' && (myArray[2] == '0')) {
             myArray[0] = '';
             myArray[1] = '';
             myArray[2] = '';
-        } if((myArray[0] || myArray[0] == '0') && (myArray[2])) {
+            display.textContent = `Error`;
+        } else if((myArray[0] || myArray[0] == '0') && (myArray[2])) {
             console.log('Both numbers exist');
             myArray[2] = +myArray[2];
             myArray[0] = operate(myArray[0], myArray[1], myArray[2]);
             myArray[2] = '';
             myArray[1] = `${e.target.getAttribute('id')}`;
             console.log(e.target.getAttribute('id'));
+            display.textContent = `${myArray[0]}`;
         } else if((myArray[0] || myArray[0] == '0') && (!myArray[2])) {
             myArray[1] = `${e.target.getAttribute('id')}`;
             myArray[0] = +myArray[0];
@@ -86,11 +101,12 @@ operators.forEach(operator => {
 });
 
 equals.addEventListener('click', () => {
-    if(myArray[1] == '/' && (myArray[2])) {
-        console.log('Error');
+    if(myArray[1] == '/' && (myArray[2] == '0')) {
+        // myArray.map(item => item = '';);
         myArray[0] = '';
         myArray[1] = '';
         myArray[2] = '';
+        display.textContent = `Error`;
     } if((myArray[0] || myArray[0] == '0') && (myArray[2])) {
         console.log('Both numbers exist, this is equals');
         myArray[2] = +myArray[2];
@@ -98,8 +114,40 @@ equals.addEventListener('click', () => {
         myArray[2] = '';
         myArray[1] = '';
         console.log(myArray);
+        display.textContent = `${myArray[0]}`;
     } else if((myArray[0]) && (!myArray[2])) {
+        display.textContent = `${myArray[0]}`;
         console.log(myArray[0]);
         console.log(myArray);
+    }
+});
+
+clearBtn.addEventListener('click', () => {
+    myArray[0] = '0';
+    myArray[1] = '';
+    myArray[2] = '';
+    display.textContent = myArray[0];
+    console.log(myArray);
+});
+
+backspaceBtn.addEventListener('click', () => {
+    if(typeof(myArray[0]) != 'number') {
+        myArray[0] = myArray[0].slice(0,-1);
+        display.textContent = `${myArray[0]}`;
+            if(!myArray[0]) {
+                myArray[0] = '0';
+                display.textContent = myArray[0];
+            }
+    } else if((typeof(myArray[0]) == 'number') && (!myArray[2])) {
+        myArray[0] = myArray[0].toString().slice(0,-1);
+        display.textContent = `${myArray[0]}`;
+        myArray[0] = +myArray[0];
+            if(!myArray[0]) {
+                display.textContent = `0`;
+                myArray[0] = '';
+            }
+    } else {
+        myArray[2] = myArray[2].slice(0,-1);
+        display.textContent = `${myArray[2]}`;
     }
 });
