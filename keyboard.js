35178f970158f0f2cb56025517ec3a0484ec0bac
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-08-13 17:21:10 +0300

    Change digit input limit using keyboard

--- keyboard.js
+++ keyboard.js
@@ -7,7 +7,7 @@ window.addEventListener('keydown', (e) => {
             if(myArray[0] == '0' || (typeof(myArray[0]) == 'number') || (myArray[0].includes('e')) || myArray[0] == '-0') { 
                 myArray[0] = '';
             }
-            if(myArray[0].toString().length > 7) {                         
+            if(myArray[0].toString().length > 11) {                         
                 return;
             }
             myArray[0] += digit.textContent;
@@ -17,7 +17,7 @@ window.addEventListener('keydown', (e) => {
             if(myArray[2] == '0' || myArray[2] == '-0') {
                 myArray[2] = '';
             }
-            if(myArray[2].toString().length > 7) {                        
+            if(myArray[2].toString().length > 11) {                        
                 return;
             }
             myArray[2] += digit.textContent;
